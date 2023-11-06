41d53644d7f2b74bed7fb8eaa66f2a90f80d07da
frectonz <fraol0912@gmail.com>
2023-11-06 23:47:04 +0300

    big bang

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+  };
+  outputs = { self, flake-utils, opam-nix, nixpkgs }:
+    let package = "watchbox";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        on = opam-nix.lib.${system};
+
+        devPackagesQuery = {
+          ocaml-lsp-server = "*";
+          ocamlformat = "*";
+        };
+        query = devPackagesQuery // {
+          ocaml-base-compiler = "*";
+        };
+
+        scope = on.buildOpamProject' { } ./. query;
+        overlay = final: prev: {
+          ${package} = prev.${package}.overrideAttrs (_: {
+            doNixSupport = false;
+          });
+        };
+
+        scope' = scope.overrideScope' overlay;
+        main = scope'.${package};
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in
+      {
+        legacyPackages = scope';
+        packages.default = main;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = devPackages ++ [ ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
