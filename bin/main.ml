f4ae45583c618bc64b1782bcf551a9ed61d7e9be
frectonz <fraol0912@gmail.com>
2023-11-07 23:04:58 +0300

    feat: impl keyboard navigation

--- bin/main.ml
+++ bin/main.ml
@@ -20,6 +20,8 @@ let title =
 |}
 ;;
 
+type app = { show_idx : int }
+
 let dirs dir =
   Sys.readdir dir
   |> Array.to_list
@@ -27,10 +29,12 @@ let dirs dir =
   |> List.filter (fun entry -> Sys.is_directory (dir ^ "/" ^ entry))
 ;;
 
-let ui_of_dirs dirs =
+let ui_of_dirs app dirs =
   dirs
-  |> List.map (fun dir -> Lwd.var (W.printf "[%s]" dir))
-  |> List.map Lwd.get
+  |> List.mapi (fun i dir ->
+    let app = Lwd.get app in
+    let attr = Lwd.map app ~f:(fun a -> if a.show_idx = i then Some A.(fg blue ++ st bold) else None) in
+    Lwd.map attr ~f:(fun attr -> W.printf ?attr "[%s]" dir))
   |> W.vlist ~bullet:"* "
 ;;
 
@@ -38,10 +42,32 @@ let () =
   if Array.length Sys.argv <> 2
   then print_endline "Usage: <path_to_directory>"
   else (
+    let app = Lwd.var { show_idx = 0 } in
     let path = Sys.argv.(1) in
     let () = Printf.printf "You provided the directory path: %s\n" path in
-    let dirs_ui = dirs path |> ui_of_dirs in
+    let dirs_lst = dirs path in
+    let dirs_ui = ui_of_dirs app dirs_lst in
     let title_ui = W.string ~attr:A.(fg blue ++ st bold) title |> Lwd.var |> Lwd.get in
     let ui = W.vbox [ title_ui; dirs_ui ] in
+    let ui =
+      Lwd.map ui ~f:(fun ui ->
+        Ui.keyboard_area
+          (function
+            | `Arrow `Up, _ ->
+              let got = Lwd.peek app in
+              let new_idx = if got.show_idx = 0 then 0 else got.show_idx - 1 in
+              let new_app = { show_idx = new_idx } in
+              let () = Lwd.set app new_app in
+              `Handled
+
+            | `Arrow `Down, _ ->
+              let got = Lwd.peek app in
+              let new_idx = if got.show_idx + 1 = List.length dirs_lst then List.length dirs_lst - 1 else got.show_idx + 1 in
+              let new_app = { show_idx = new_idx } in
+              let () = Lwd.set app new_app in
+              `Handled
+            | _ -> `Unhandled)
+          ui)
+    in
     Ui_loop.run ui ~quit_on_escape:true)
 ;;
